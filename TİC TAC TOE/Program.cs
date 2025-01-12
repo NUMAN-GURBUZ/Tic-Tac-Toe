48a326151b3027939d018b2e0ef367fd8d7a1c99
NUMAN GÜRBÜZ <146639528+NUMAN-GURBUZ@users.noreply.github.com>
2025-01-12 18:59:51 +0300

    Tic-Tac-Toe
    c# devexpress game

---
+++ TİC TAC TOE/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.UserSkins;
+using DevExpress.Skins;
+using DevExpress.LookAndFeel;
+
+namespace TİC_TAC_TOE
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            BonusSkins.Register();
+            Application.Run(new Form1());
+        }
+    }
+}
